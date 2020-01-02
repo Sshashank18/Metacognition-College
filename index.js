8d23b8daccc26edc3ede567e94eabdeeaaf49066
Shashank aggarwal <shashankaggarwal13@gmail.com>
2020-01-03 01:45:36 +0530

    Navbar is functional

---
+++ index.js
@@ -0,0 +1,13 @@
+let navbar=$('#navbar');
+let btncont=$('#btn-container');
+let cross=$('#cross');
+
+$('#menu').click(()=>{
+    navbar.removeAttr("hidden");
+    btncont.attr("hidden",true);
+})
+
+cross.click(()=>{
+    navbar.attr("hidden",true);
+    btncont.removeAttr("hidden");
+})
