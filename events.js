75a20ea277d9fd6184badf293b30ba908eed0845
Shashank aggarwal <shashankaggarwal13@gmail.com>
2020-02-04 00:30:45 +0530

    eve

--- events.js
+++ events.js
@@ -184,3 +184,25 @@ let rgbtn=$('.rgbtn');
 rgbtn.click((event)=>{
     window.location="https://metacognition-registration.herokuapp.com/";
 });
+
+
+
+let navbar=$('#navbar');
+let btncont=$('#btn-container');
+let cross=$('#cross');
+let navHit=$('.navHit');
+
+$('#menu').click(()=>{
+    navbar.removeAttr("hidden");
+    btncont.attr("hidden",true);
+})
+
+cross.click(()=>{
+    navbar.attr("hidden",true);
+    btncont.removeAttr("hidden");
+})
+
+navHit.click(()=>{
+    navbar.attr("hidden",true);
+    btncont.removeAttr("hidden");
+})
